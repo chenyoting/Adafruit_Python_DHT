46c0985c2a5d1088290c7d4ebb7cc2e2f1a570db
chenyoting <doris11.2533@gmail.com>
2017-12-11 19:07:27 +0800

    led

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -19,70 +19,68 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
 import time
 import sys
 import httplib, urllib
 import json
-import Adafruit_DHT
+
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+import Adafruit_DHT
 deviceId = "DMa8dd26"
 deviceKey = "etiMUmvm4nB0cQsk"
+
+
 def post_to_mcs(payload):
-	headers = {"Content-type": "application/json", "deviceKey": deviceKey}
-	not_connected = 1
-	while (not_connected):
-		try:
-			conn = httplib.HTTPConnection("api.mediatek.com:80")
-			conn.connect()
-			not_connected = 0
-		except (httplib.HTTPException, socket.error) as ex:
-			print "Error: %s"
-#% ex time.sleep(10)
-			 # sleep 10 seconds
-	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers)
-	response = conn.getresponse()
-	print( response.status, response.reason, json.dumps(payload), time.strftime("%c"))
-	data = response.read()
-	conn.close()
-
-
+ 	headers = {"Content-type": "application/json", "deviceKey": deviceKey}
+ 	not_connected = 1
+ 	while (not_connected):
+ 		try:
+ 			conn = httplib.HTTPConnection("api.mediatek.com:80")
+ 			conn.connect()
+ 			not_connected = 0
+ 		except (httplib.HTTPException, socket.error) as ex:
+ 			print "Error: %s"
+ 	# % ex time.sleep(10)
+ 			 # sleep 10 seconds
+ 	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers)
+ 	response = conn.getresponse()
+ 	print( response.status, response.reason, json.dumps(payload), time.strftime("%c"))
+ 	data = response.read()
+ 	conn.close()
 
 # Parse command line parameters.
 sensor_args = { '11': Adafruit_DHT.DHT11,
-                '22': Adafruit_DHT.DHT22,
-                '2302': Adafruit_DHT.AM2302 }
+                 '22': Adafruit_DHT.DHT22,
+                 '2302': Adafruit_DHT.AM2302 }
 if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-    sensor = sensor_args[sys.argv[1]]
-    pin = sys.argv[2]
+     sensor = sensor_args[sys.argv[1]]
+     pin = sys.argv[2]
 else:
-    print('usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#')
-    print('example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4')
-    sys.exit(1)
+     print('usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#')
+     print('example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4')
+     sys.exit(1)
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-
-
 while True:
-	h0, t0= Adafruit_DHT.read_retry(sensor, pin)
+
 	SwitchStatus = GPIO.input(24)
 	if( SwitchStatus == 0):
 		print('Button pressed')
 	else:
 		print('Button released')
+ 	h0, t0= Adafruit_DHT.read_retry(sensor, pin)
+ 	if h0 is not None and t0 is not None:
+ 		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(t0, h0))
 
-	if h0 is not None and t0 is not None:
-		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(t0, h0))
-
-		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
-			{"dataChnId":"Temperature","values":{"value":t0}},
-			{"dateChnId":"SwitchStatus","values":{"value":SwitchStatus}}]}
-		post_to_mcs(payload)
+		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},{"dataChnId":"Temperature","values":{"value":t0}}]}
+		payload = {"datapoints":[
+			  {"dataChnId":"Humidity","values":{"value":h0}},
+			  {"dataChnId":"Temperature","values":{"value":t0}},
+			  {"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]}
+ 		post_to_mcs(payload)
 		time.sleep(10)
-
-	else:
-		print('Failed to get reading. Try again!')
-sys.exit(1)
 
